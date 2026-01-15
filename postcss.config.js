c74b341d1dd3843ad505deee717efb4b5a96c647
otTATto <TATo123SU@gmail.com>
2026-01-15 18:24:14 +0900

    fix: Tailwind CSS が生成する rem 単位を px 単位に変換

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,13 @@
 export default {
   plugins: {
-    tailwindcss: {},
+    tailwindcss: {},    
+    'postcss-rem-to-responsive-pixel': {
+      rootValue: 16,        // 1rem = 16px
+      propList: ['*'],      // 全プロパティ変換
+      transformUnit: 'px',  // px出力
+      replace: true,        // remを置き換え
+      mediaQuery: true      // メディアクエリ内も変換
+    },
     autoprefixer: {}
   }
 }
